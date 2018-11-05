1a28196edd3fcd38bde3c011ad897e418c7a223c
Dell\Home <stefo.services@op.pl>
2018-11-05 22:27:33 +0100

    zajęcia 1: Rozbudowa pliku p1zgadywanka.py

--- Przyklady/p1zgadywanka.py
+++ Przyklady/p1zgadywanka.py
@@ -3,22 +3,31 @@
 
 import random
 
-liczba = random.randint(1, 49)
-#print("Wylosowana liczba: ", liczba)
+ileliczb = int(input("Podaj ilość typowanych liczb: "))
+maksliczba = int(input("Podaj maksymalną losowaną liczbę: "))
+print("Wytypuj %s z %s liczb: " % (ileliczb, maksliczba))
 
 
-for i in range(6):
-    print("Próba ", i+1)
-    odp = input("Jaka liczbę od 1 do 49 wylosowano: ")
+liczba=[]
 
-    if liczba == int(odp):
-        print("Hura!!!")
-        break
-    elif i==5:
-        print("wyslowano: ", liczba)
-    else:
-        print("No niestety")
-        if int(odp) > liczba:
-            print("Wyslosowana liczba jest mniejsza od: ", odp)
+for x in range(ileliczb):
+    liczba.append(random.randint(1, maksliczba))
+
+for n in range(ileliczb):
+    print("Liczba %s z %s" % (n+1,ileliczb))
+
+    for i in range(6):
+        print("Próba ", i+1)
+        odp = int(input("Jaka liczbę od 1 do "+str(maksliczba)+" wyslowoano"))
+
+        if liczba[n] == int(odp):
+            print("Hura!!!")
+            break
+        elif i==5:
+            print("wyslowano: ", liczba[n])
         else:
-            print("Wyslosowana liczba jest większa od: ", odp)
+            print("No niestety")
+            if int(odp) > liczba[n]:
+                print("Wyslosowana liczba jest mniejsza od: ", odp)
+            else:
+                print("Wyslosowana liczba jest większa od: ", odp)

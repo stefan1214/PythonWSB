14f0104eea5cc8caee4f6222da7370c01b414424
Dell\Home <stefo.services@op.pl>
2018-11-28 15:09:19 +0100

    zaj 3 mapa konturowa

---
+++ zaj3/z3p11.py
@@ -0,0 +1,19 @@
+from pylab import *
+
+
+def f(x, y):
+
+
+    return exp(-x ** 2 - y ** 2 / 4) + \
+           exp(-(x + 2) ** 2 - y ** 2 / 2) + \
+           exp(-(x - 2) ** 2 - 2 * y ** 2)
+n = 256
+z = 3.5
+x = linspace(-z, z, n)
+y = linspace(-z, z, n)
+X, Y = meshgrid(x, y)
+contourf(X, Y, f(X, Y), 8)
+C = contour(X, Y, f(X, Y), 8, colors='b', linewidths=1)
+clabel(C, inline=1, fontsize=10)
+savefig("kontur .png")
+show()

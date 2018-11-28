4ca39466b9373d054c91b6011d9718e0aa2ed4f6
Dell\Home <stefo.services@op.pl>
2018-11-28 14:49:00 +0100

    zaj 3 Trygo wykres ulepszony

--- zaj3/z3p5.py
+++ zaj3/z3p5.py
@@ -1,12 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-#Funkcja arange jest podobna do standardowej funkcji range wytwarzającej określone sekwencje w postaci listy.
+# Funkcja arange jest podobna do standardowej funkcji range wytwarzającej określone sekwencje w postaci listy.
 # Funkcja arange zamiast listy wytwarza tablicę zawierającą ciąg liczb zmiennoprzecinkowych
 # zaczynający się od pierwszego podanego argumentu funkcji arange 0 do ostatniego 2 w kroku co 0.01 (0.00, 0.01, … 1.99,2.00)
 
 x = np.arange(0.0, 2.0, 0.01)
 y = np.sin(2.0 * np.pi * x)
-plt.plot(x, y)
+plt.plot(x, y, 'r:', linewidth=6)
+
+plt.xlabel('Czas')
+plt.ylabel('Pozycja')
+plt.title('Nasz pierwszy wykres')
+plt.grid(True)
+
 plt.show()

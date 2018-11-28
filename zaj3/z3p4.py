4e109a1820c18a0895fe5728e7e4f3d64a1e895b
Dell\Home <stefo.services@op.pl>
2018-11-28 14:41:10 +0100

    zaj 3 Matplotlib 2 + komentarze

--- zaj3/z3p4.py
+++ zaj3/z3p4.py
@@ -1,24 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# ustawiania domyślne
 plt.figure(figsize=(10, 6), dpi=80)
 plt.subplot(1, 1, 1)
 
+# parametry lini
 X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
 C, S = np.cos(X), np.sin(X)
 
+# zakresy
 plt.plot(X, C, color="blue", linewidth=2.5, linestyle="-")
 plt.plot(X, S, color="red", linewidth=2.5, linestyle="-")
 
+# zmienność
 plt.xlim(X.min() * 1.1, X.max() * 1.1)
 plt.ylim(C.min() * 1.1, C.max() * 1.1)
 
+# zmiana wartości na osiach
 plt.xticks([-np.pi, -np.pi / 2, 0, np.pi / 2, np.pi],
            [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
 
 plt.yticks([-1, 0, +1],
            [r'$-1$', r'$0$', r'$+1$'])
 
+# pozycjonowanie osi liczbowych
 ax = plt.gca()
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
@@ -27,11 +33,13 @@ ax.spines['bottom'].set_position(('data', 0))
 ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data', 0))
 
+# legenda
 plt.plot(X, C, color="blue", linewidth=2.5, linestyle="-", label="cosine")
 plt.plot(X, S, color="red", linewidth=2.5, linestyle="-", label="sine")
 
 plt.legend(loc='upper left', frameon=False)
 
+# sprawdzanie wybranych punktów
 t = 2 * np.pi / 3
 plt.plot([t, t], [0, np.cos(t)], color='blue', linewidth=1.5, linestyle="--")
 plt.scatter([t, ], [np.cos(t), ], 50, color='blue')
@@ -49,9 +57,9 @@ plt.annotate(r'$\cos(\frac{2\pi}{3})=-\frac{1}{2}$',
              xytext=(-90, -50), textcoords='offset points', fontsize=16,
              arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
 
+# uwydatnianie etykiet
 for label in ax.get_xticklabels() + ax.get_yticklabels():
     label.set_fontsize(16)
     label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.65))
-
 
 plt.show()

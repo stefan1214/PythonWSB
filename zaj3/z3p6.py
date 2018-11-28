2f31d3bc4d1c01936e0b6965d700d6cc843237b6
Dell\Home <stefo.services@op.pl>
2018-11-28 14:51:51 +0100

    zaj 3 Trygo kilka wykresów 2

--- zaj3/z3p6.py
+++ zaj3/z3p6.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 x = np.arange(0.0, 2.0, 0.01)
 y1 = np.sin(2.0 * np.pi * x)
 y2 = np.cos(2.0 * np.pi * x)
-plt.plot(x, y1, 'r:', x, y2, 'g')
-plt.legend(('dane y1', 'dane y2'))
+y = y1 * y2
+l1, = plt.plot(x, y, 'b')
+l2, l3 = plt.plot(x, y1, 'r:', x, y2, 'g')
+plt.legend((l2, l3, l1), ('dane y1', 'dane y2', 'y1*y2'))
 plt.xlabel('Czas')
 plt.ylabel('Pozycja')
 plt.title('Wykres ')

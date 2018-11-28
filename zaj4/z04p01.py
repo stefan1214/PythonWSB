3bdbcbba6980e11925cef2ef75d0cc28a85e6ba9
Dell\Home <stefo.services@op.pl>
2018-11-28 19:21:05 +0100

    zadania zaj4 kalkulator 3

--- zaj4/z04p01.py
+++ zaj4/z04p01.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QLabel, QGridLayout
+from PyQt5.QtWidgets import QLineEdit, QPushButton, QHBoxLayout
 
 
 class Kalkulator(QWidget):
@@ -22,6 +23,34 @@ class Kalkulator(QWidget):
         ukladT.addWidget(etykieta3, 0, 2)
 
         # przypisanie utworzonego
+
+        self.liczba1Edt = QLineEdit()
+        self.liczba2Edt = QLineEdit()
+        self.wynikEdt = QLineEdit()
+
+        self.wynikEdt.readonly = True
+        self.wynikEdt.setToolTip('Wpisz <b>liczby</b> i wybierz działanie...')
+
+        ukladT.addWidget(self.liczba1Edt, 1, 0)
+        ukladT.addWidget(self.liczba2Edt, 1, 1)
+        ukladT.addWidget(self.wynikEdt, 1, 2)
+
+        dodajBtn = QPushButton("&Dodaj", self)
+        odejmijBtn = QPushButton("&Odejmij", self)
+        dzielBtn = QPushButton("&Mnóż", self)
+        mnozBtn = QPushButton("D&ziel", self)
+        koniecBtn = QPushButton("&Koniec", self)
+        koniecBtn.resize(koniecBtn.sizeHint())
+
+        ukladH = QHBoxLayout()
+        ukladH.addWidget(dodajBtn)
+        ukladH.addWidget(odejmijBtn)
+        ukladH.addWidget(dzielBtn)
+        ukladH.addWidget(mnozBtn)
+
+        ukladT.addLayout(ukladH, 2, 0, 1, 3)
+        ukladT.addWidget(koniecBtn, 3, 0, 1, 3)
+
         self.setLayout(ukladT)
 
         self.setGeometry(20, 20, 300, 100)

207c7944df0f79a4fc2bc9e28169399fcf10fdcb
Dell\Home <stefo.services@op.pl>
2018-12-01 11:31:28 +0100

    zadania zaj4 kalkulator 4

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 "# PythonWSB"  
+http://technikum.zse.com.pl/wsb/zaj1.pdf
+http://technikum.zse.com.pl/wsb/zaj2.pdf
+http://technikum.zse.com.pl/wsb/zaj3.pdf
+http://technikum.zse.com.pl/wsb/zaj4.pdf
+
 terminal: pip freeze > requirements.txt export bibliotek do pliku  
 terminal: pip install -r requirements.txt import bibliotek z pliku  
--- zaj4/z04p01.py
+++ zaj4/z04p01.py
@@ -2,28 +2,31 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QLabel, QGridLayout
 from PyQt5.QtWidgets import QLineEdit, QPushButton, QHBoxLayout
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import Qt
 
 
 class Kalkulator(QWidget):
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.interfejs()
 
     def interfejs(self):
+
         # etykiety
         etykieta1 = QLabel("Liczba 1:", self)
         etykieta2 = QLabel("Liczba 2:", self)
         etykieta3 = QLabel("Wynik:", self)
 
-        # przypisanie widgetów
+        # przypisanie widgetów do układu tabelarycznego
         ukladT = QGridLayout()
         ukladT.addWidget(etykieta1, 0, 0)
         ukladT.addWidget(etykieta2, 0, 1)
         ukladT.addWidget(etykieta3, 0, 2)
 
-        # przypisanie utworzonego
-
+        # 1-liniowe pola edycyjne
         self.liczba1Edt = QLineEdit()
         self.liczba2Edt = QLineEdit()
         self.wynikEdt = QLineEdit()
@@ -35,6 +38,7 @@ class Kalkulator(QWidget):
         ukladT.addWidget(self.liczba2Edt, 1, 1)
         ukladT.addWidget(self.wynikEdt, 1, 2)
 
+        # przyciski
         dodajBtn = QPushButton("&Dodaj", self)
         odejmijBtn = QPushButton("&Odejmij", self)
         dzielBtn = QPushButton("&Mnóż", self)
@@ -51,12 +55,67 @@ class Kalkulator(QWidget):
         ukladT.addLayout(ukladH, 2, 0, 1, 3)
         ukladT.addWidget(koniecBtn, 3, 0, 1, 3)
 
+        # przypisanie utworzonego układu do okna
         self.setLayout(ukladT)
 
+        koniecBtn.clicked.connect(self.koniec)
+        dodajBtn.clicked.connect(self.dzialanie)
+        odejmijBtn.clicked.connect(self.dzialanie)
+        mnozBtn.clicked.connect(self.dzialanie)
+        dzielBtn.clicked.connect(self.dzialanie)
+
+        self.liczba1Edt.setFocus()
         self.setGeometry(20, 20, 300, 100)
         self.setWindowIcon(QIcon('kalkulator.png'))
         self.setWindowTitle("Prosty kalkulator")
         self.show()
+
+    def koniec(self):
+        self.close()
+
+    def closeEvent(self, event):
+
+        odp = QMessageBox.question(
+            self, 'Komunikat',
+            "Czy na pewno koniec?",
+            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if odp == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+
+    def dzialanie(self):
+
+        nadawca = self.sender()
+
+        try:
+            liczba1 = float(self.liczba1Edt.text())
+            liczba2 = float(self.liczba2Edt.text())
+            wynik = ""
+
+            if nadawca.text() == "&Dodaj":
+                wynik = liczba1 + liczba2
+            elif nadawca.text() == "&Odejmij":
+                wynik = liczba1 - liczba2
+            elif nadawca.text() == "&Mnóż":
+                wynik = liczba1 * liczba2
+            else:  # dzielenie
+                try:
+                    wynik = round(liczba1 / liczba2, 9)
+                except ZeroDivisionError:
+                    QMessageBox.critical(
+                        self, "Błąd", "Nie można dzielić przez zero!")
+                    return
+
+            self.wynikEdt.setText(str(wynik))
+
+        except ValueError:
+            QMessageBox.warning(self, "Błąd", "Błędne dane", QMessageBox.Ok)
 
 
 if __name__ == '__main__':
